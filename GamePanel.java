7684750f722f11f342b1ae6a860660799186bbad
aakash12516 <akashk12516@gmail.com>
2023-04-17 22:05:02 +0530

    commit

--- GamePanel.java
+++ GamePanel.java
@@ -51,7 +51,7 @@ public class GamePanel extends JPanel implements Runnable{
 		paddle2.draw(g);
 		ball.draw(g);
 		score.draw(g);
-Toolkit.getDefaultToolkit().sync(); // I forgot to add this line of code in the video, it helps with the animation
+Toolkit.getDefaultToolkit().sync(); //  it helps with the animation
 
 	}
 	public void move() {
